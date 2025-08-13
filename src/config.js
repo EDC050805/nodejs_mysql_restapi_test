4c01952830611ca979047ec3ff0715fd0c7661a5
Elias Diaz <eliasalejandrodc@hotmail.com>
2025-08-13 01:05:39 -0500

    Agregando comentarios descriptivos

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ import {config} from 'dotenv'
 config()
 //"process" es un objeto global de node, "env" almacena todas las variables que tiene el computador y PORT es una de esas variables
 //El primero es el puerto 3000 que es del servidor y el segundoe es del puerto de la base de datos
-//mysql://root:oAQVABpHSiZtFaxjRVlrfEOvAJaHOWaK@mysql.railway.internal:3306/railway
+//URL local para la conexión con railway: mysql://root:oAQVABpHSiZtFaxjRVlrfEOvAJaHOWaK@mysql.railway.internal:3306/railway
+//URL pública para la conexión de la base de datos de railway con Mysql workbench: mysql://root:oAQVABpHSiZtFaxjRVlrfEOvAJaHOWaK@turntable.proxy.rlwy.net:11170/railway
 export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
